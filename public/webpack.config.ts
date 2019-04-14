f39c83665951ff4a9b4fcfff499703f9adcdef4d
Alexander Leung <alex.l.leung@gmail.com>
2019-04-14 01:02:13 -0700

    Beginning to add in public API file structure, which webpack will compile to a set of release files which can be published on npm.

---
+++ public/webpack.config.ts
@@ -0,0 +1,11 @@
+import * as webpack from 'webpack';
+import * as fs from 'fs';
+
+// Recursive read of the './source' directory.
+// Each file is its own webpack bundle, which will be output to the dist folder.
+
+module.exports = [
+    {
+
+    }
+];
